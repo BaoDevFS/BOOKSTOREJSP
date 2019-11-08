1c13eb5d8afa457041b0da2b2c30a63ef1da37a5
BaoNguyen <17130010@st.hcmuaf.edu.vn>
2019-11-08 16:06:29 +0700

    updade admin

---
+++ src/vn/edu/nlu/servletad/Index.java
@@ -0,0 +1,25 @@
+package vn.edu.nlu.servletad;
+import vn.edu.nlu.fit.model.Users;
+import vn.edu.nlu.git.database.GetConnectDatabase;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+@WebServlet("/Index")
+public class Index extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/admin/pages/index.jsp");
+        requestDispatcher.forward(request,response);
+    }
+}
