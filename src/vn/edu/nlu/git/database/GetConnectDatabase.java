a41e7eb464fb62d8e47d98db1ec464a9c59c971d
BaoNguyen <17130010@st.hcmuaf.edu.vn>
2019-11-21 22:31:10 +0700

    update database and images

--- src/vn/edu/nlu/git/database/GetConnectDatabase.java
+++ src/vn/edu/nlu/git/database/GetConnectDatabase.java
@@ -12,7 +12,7 @@ public class GetConnectDatabase {
             e.printStackTrace();
         }
         String hostName ="localhost";
-        String dbName ="book";
+        String dbName ="bqg";
         String userName="root";
         String password="";
         String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName+"?useUnicode=true&characterEncoding=utf-8";
