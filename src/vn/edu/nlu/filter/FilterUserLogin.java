be793475fda01f8731a62526b7e5079eb50386ae
BaoNguyen <17130010@st.hcmuaf.edu.vn>
2019-12-09 22:54:16 +0700

    gtyu

---
+++ src/vn/edu/nlu/filter/FilterUserLogin.java
@@ -0,0 +1,33 @@
+package vn.edu.nlu.filter;
+
+import vn.edu.nlu.fit.model.Users;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName = "FilterUserLogin",urlPatterns = {"/AjaxCart","/Checkout"})
+public class FilterUserLogin implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpSession session = request.getSession();
+        Users users = (Users) session.getAttribute("user");
+        if(users!=null&& users.getEmail()!=null){
+            chain.doFilter(req, resp);
+        }else{
+            response.sendRedirect(request.getContextPath() + "/Login");
+        }
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
