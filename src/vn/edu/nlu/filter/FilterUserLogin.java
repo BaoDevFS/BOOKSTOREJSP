c1c24d53b4c6e3cac50bbefb6804e6476a14d324
BaoNguyen <17130010@st.hcmuaf.edu.vn>
2019-12-11 10:08:18 +0700

    abc

--- src/vn/edu/nlu/filter/FilterUserLogin.java
+++ src/vn/edu/nlu/filter/FilterUserLogin.java
@@ -17,11 +17,9 @@ public class FilterUserLogin implements Filter {
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
-        System.out.println("AjaxCart");
         HttpSession session = request.getSession();
         Users users = (Users) session.getAttribute("user");
         if(users!=null&& users.getEmail()!=null){
-            System.out.println("@"+users.toString());
             chain.doFilter(req, resp);
         }else{
             response.sendRedirect(request.getContextPath() + "/Login");
