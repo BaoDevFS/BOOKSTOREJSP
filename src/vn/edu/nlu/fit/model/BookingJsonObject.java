d8ca3c3e371de284fe311342f9c03d514b216f9f
quyen <lelamphuongquyen@gmail.com>
2020-01-09 00:01:56 +0700

    q

---
+++ src/vn/edu/nlu/fit/model/BookingJsonObject.java
@@ -0,0 +1,15 @@
+package vn.edu.nlu.fit.model;
+
+import java.util.ArrayList;
+
+public class BookingJsonObject {
+    ArrayList<Orders> orders;
+
+    public ArrayList<Orders> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(ArrayList<Orders> orders) {
+        this.orders = orders;
+    }
+}
